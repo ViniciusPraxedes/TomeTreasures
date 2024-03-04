9a4842c614ef9a74653e8fa2358db32137c575c6
Vinicius Praxedes <vinicius.praxedes@nexergroup.com>
2024-03-04 15:07:45 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,32 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import {createBrowserRouter, Outlet, RouterProvider} from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
-import Products from "./pages/Products/Products";
-import Product from "./pages/Product/Product";
-import "./app.scss"
-import Contact from "./components/Contact/Contact";
-import CategoryBar from "./components/CategoryBar/CategoryBar";
-import Success from "./pages/Payment/Success";
-import Cancel from "./pages/Payment/Cancel";
+import Home from './pages/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Products from './pages/Products/Products';
+import Product from './pages/Product/Product';
+import Contact from './components/Contact/Contact';
+import CategoryBar from './components/CategoryBar/CategoryBar';
+import Success from './pages/Payment/Success';
+import Cancel from './pages/Payment/Cancel';
 
-const Layout = () =>{
-    return(
-        <div className="app">
-            <Navbar></Navbar>
-            <CategoryBar/>
-            <Outlet></Outlet>
-            <Contact/>
-            <Footer></Footer>
-        </div>
-    )
-}
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Layout/>,
-        children: [
-            {
-                path: "/",
-                element: <Home/>
-            },
-            {
-                path: "/products/:category",
-                element: <Products/>
-            },
-            {
-                path: "/product/:id",
-                element: <Product/>
-            }
-        ]
-    },
-    {
-        path: "/success",
-        element: <Success/>
-    },
-    {
-        path: "/cancel",
-        element: <Cancel/>
-    }
-]);
-
-function App() {
+const App = () => {
     return (
         <div className="App">
-            <RouterProvider router={router}></RouterProvider>
+            <Router>
+                <Navbar />
+                <CategoryBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products/:category" element={<Products />} />
+                    <Route path="/product/:id" element={<Product />} />
+                    <Route path="/success" element={<Success />} />
+                    <Route path="/cancel" element={<Cancel />} />
+                </Routes>
+                <Contact />
+                <Footer />
+            </Router>
         </div>
     );
 }
