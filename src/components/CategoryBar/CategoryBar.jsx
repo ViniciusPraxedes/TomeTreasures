4b904ce9cb88028a6900f3b4074e17d85b4e05b8
Vinicius Praxedes <vinicius.praxedes@nexergroup.com>
2024-03-06 22:24:24 +0100

    Debug

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -8,27 +8,27 @@ const CategoryBar = () => {
         <div className="category">
             <div className="wrapper">
                 <div className="items">
-                    <Link onClick={() => {window.location.href="/products/best_seller"}} >Best seller</Link>
-                    <Link onClick={() => {window.location.href="/products/trending"}} >Trending</Link>
-                    <Link onClick={() => {window.location.href="/products/science_fiction"}} >Sci-Fi</Link>
-                    <Link onClick={() => {window.location.href="/products/mystery"}} >Mystery</Link>
-                    <Link onClick={() => {window.location.href="/products/fantasy"}} >Fantasy</Link>
-                    <Link onClick={() => {window.location.href="/products/history"}} >History</Link>
-                    <Link onClick={() => {window.location.href="/products/poetry"}} >Poetry</Link>
-                    <Link onClick={() => {window.location.href="/products/romance"}} >Romance</Link>
+                    <Link  to="/products/best_seller">Best seller</Link>
+                    <Link  to="/products/trending">Trending</Link>
+                    <Link  to="/products/science_fiction">Sci-Fi</Link>
+                    <Link  to="/products/mystery">Mystery</Link>
+                    <Link  to="/products/fantasy">Fantasy</Link>
+                    <Link  to="/products/history">History</Link>
+                    <Link  to="/products/poetry">Poetry</Link>
+                    <Link  to="/products/romance">Romance</Link>
                 </div>
             </div>
             <div className="dropdown">
                 <button className="dropdown-button">Categories<KeyboardArrowDownIcon className="arrow"/></button>
                 <div className="dropdown-content">
-                    <Link onClick={() => {window.location.href="/products/best_seller"}} >Best seller</Link>
-                    <Link onClick={() => {window.location.href="/products/trending"}} >Trending</Link>
-                    <Link onClick={() => {window.location.href="/products/science_fiction"}} >Sci-Fi</Link>
-                    <Link onClick={() => {window.location.href="/products/mystery"}} >Mystery</Link>
-                    <Link onClick={() => {window.location.href="/products/fantasy"}} >Fantasy</Link>
-                    <Link onClick={() => {window.location.href="/products/history"}} >History</Link>
-                    <Link onClick={() => {window.location.href="/products/poetry"}} >Poetry</Link>
-                    <Link onClick={() => {window.location.href="/products/romance"}} >Romance</Link>
+                    <Link to="/products/best_seller">Best seller</Link>
+                    <Link to="/products/trending">Trending</Link>
+                    <Link  to="/products/science_fiction">Sci-Fi</Link>
+                    <Link  to="/products/mystery">Mystery</Link>
+                    <Link  to="/products/fantasy">Fantasy</Link>
+                    <Link  to="/products/history">History</Link>
+                    <Link to="/products/poetry">Poetry</Link>
+                    <Link  to="/products/romance">Romance</Link>
                 </div>
             </div>
         </div>
