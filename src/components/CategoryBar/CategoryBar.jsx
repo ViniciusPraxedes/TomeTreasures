8c11a93b3c15f1e6dc38881e6e042fb0491c26d2
Vinicius Praxedes <vinicius.praxedes@nexergroup.com>
2024-03-07 09:48:19 +0100

    debug

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -26,13 +26,69 @@ const CategoryBar = () => {
                               window.location.reload();
                               window.location.href = "/products/best_seller";
                           }} >Best seller</Link>
-                    <Link to="/products/trending">Trending</Link>
-                    <Link  to="/products/science_fiction">Sci-Fi</Link>
-                    <Link  to="/products/mystery">Mystery</Link>
-                    <Link  to="/products/fantasy">Fantasy</Link>
-                    <Link  to="/products/history">History</Link>
-                    <Link to="/products/poetry">Poetry</Link>
-                    <Link  to="/products/romance">Romance</Link>
+                    <Link
+                        to="/products/trending"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/trending";
+                        }}
+                    >
+                        Trending
+                    </Link>
+                    <Link
+                        to="/products/science_fiction"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/science_fiction";
+                        }}
+                    >
+                        Sci-Fi
+                    </Link>
+                    <Link
+                        to="/products/mystery"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/mystery";
+                        }}
+                    >
+                        Mystery
+                    </Link>
+                    <Link
+                        to="/products/fantasy"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/fantasy";
+                        }}
+                    >
+                        Fantasy
+                    </Link>
+                    <Link
+                        to="/products/history"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/history";
+                        }}
+                    >
+                        History
+                    </Link>
+                    <Link
+                        to="/products/poetry"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/poetry";
+                        }}
+                    >
+                        Poetry
+                    </Link>
+                    <Link
+                        to="/products/romance"
+                        onClick={() => {
+                            window.location.reload();
+                            window.location.href = "/products/romance";
+                        }}
+                    >
+                        Romance
+                    </Link>
                 </div>
             </div>
         </div>
