43cb88b457ff17e0bb242d11e53a053d02acfbb6
Vinicius Praxedes <vinicius.praxedes@nexergroup.com>
2024-03-07 10:05:46 +0100

    debug

--- src/components/CategoryBar/CategoryBar.jsx
+++ src/components/CategoryBar/CategoryBar.jsx
@@ -21,74 +21,14 @@ const CategoryBar = () => {
             <div className="dropdown">
                 <button className="dropdown-button">Categories<KeyboardArrowDownIcon className="arrow"/></button>
                 <div className="dropdown-content">
-                    <Link to="/products/best_seller" to="/products/best_seller"
-                          onClick={() => {
-                              window.location.reload();
-                              window.location.href = "/products/best_seller";
-                          }} >Best seller</Link>
-                    <Link
-                        to="/products/trending"
-                        onClick={() => {
-
-                            window.location.href = "/products/trending";
-                        }}
-                    >
-                        Trending
-                    </Link>
-                    <Link
-                        to="/products/science_fiction"
-                        onClick={() => {
-                            window.location.reload();
-                            window.location.href = "/products/science_fiction";
-                        }}
-                    >
-                        Sci-Fi
-                    </Link>
-                    <Link
-                        to="/products/mystery"
-                        onClick={() => {
-                            window.location.reload();
-                            window.location.href = "/products/mystery";
-                        }}
-                    >
-                        Mystery
-                    </Link>
-                    <Link
-                        to="/products/fantasy"
-                        onClick={() => {
-                            window.location.reload();
-                            window.location.href = "/products/fantasy";
-                        }}
-                    >
-                        Fantasy
-                    </Link>
-                    <Link
-                        to="/products/history"
-                        onClick={() => {
-                            window.location.reload();
-                            window.location.href = "/products/history";
-                        }}
-                    >
-                        History
-                    </Link>
-                    <Link
-                        to="/products/poetry"
-                        onClick={() => {
-                            window.location.reload();
-                            window.location.href = "/products/poetry";
-                        }}
-                    >
-                        Poetry
-                    </Link>
-                    <Link
-                        to="/products/romance"
-                        onClick={() => {
-                            window.location.reload();
-                            window.location.href = "/products/romance";
-                        }}
-                    >
-                        Romance
-                    </Link>
+                    <Link to="/products/best_seller" to="/products/best_seller" onClick={() => {window.location.href = "/products/best_seller";}} >Best seller</Link>
+                    <Link to="/products/trending" onClick={() => {window.location.href = "/products/trending";}}>Trending</Link>
+                    <Link to="/products/science_fiction" onClick={() => {window.location.href = "/products/science_fiction";}}>Sci-Fi</Link>
+                    <Link to="/products/mystery" onClick={() => {window.location.href = "/products/mystery";}}>Mystery</Link>
+                    <Link to="/products/fantasy" onClick={() => {window.location.href = "/products/fantasy";}}>Fantasy</Link>
+                    <Link to="/products/history" onClick={() => {window.location.href = "/products/history";}}>History</Link>
+                    <Link to="/products/poetry" onClick={() => {window.location.href = "/products/poetry";}}>Poetry</Link>
+                    <Link to="/products/romance" onClick={() => {window.location.href = "/products/romance";}}>Romance</Link>
                 </div>
             </div>
         </div>
